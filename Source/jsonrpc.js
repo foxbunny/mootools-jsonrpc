b7fd810df74ce914de238ce3cf1097ea344c6a85
Branko Vukelic <studio@brankovukelic.com>
2010-12-01 03:13:23 +0100

    Rewrite that extends Request.JSON

--- Source/jsonrpc.js
+++ Source/jsonrpc.js
@@ -1,82 +1,53 @@
 /*
----
-description: Implementation of JSON-RPC 1.1 for MooTools. 
-license: GPL
-authors: Branko Vukelic
-provides: [JSONRPC, JSONRPC.call]
-requires:
-  core:1.3: [Native, Class, Class.Extras, Event, Request]
-...
-*/
+ ---
+ description: Extends Request.JSON with methods to send JSON-RPC requests.
+ license: GPL
+ authors: Branko Vukelic
+ provides: [Request.JSON.rpcVersion, Request.JSON.callRPC]
+ requires:
+ core:1.3: [Request]
+ ...
+ */
 
-var JSONRPC = {};
-
-JSONRPC.version = '1.1';
-
-JSONRPC.call = new Class({
-    Implements: [Events, Options],
-    
+// Add option to specify the JSON-RPC version
+Request.JSON.extend({
     options: {
-        url: '/',
-        method: 'post',
-        encoding: 'utf-8',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json, text/x-json, application/x-javascript'
-        }
-    },
-    
-    // JSONRPC events
-    success: false,
-    failure: false,
-    
-    // ID of the last RPC call
-    rpcid: null,
-    
-    // Cached objects
-    Request: false,
-    
-    initialize: function(options){
-        this.setOptions(options);
-        this.Request = new Request.JSON({
-           url: this.options.url,
-           method: this.options.method,
-           encoding: this.options.encoding,
-           headers: this.options.headers,
-           onFailure: function(xhr){
-               this.fireEvent('failure', xhr);
-           }.bind(this),
-           onSuccess: function(response) {
-               if (response.id === this.rpcid) {
-                   this.fireEvent('success', response);
-               }
-               else {
-                   this.fireEvent('failure', null);
-               }
-           }.bind(this)
-        });
-        return this;
-    },
-    
+        rpcVersion: '1.0'
+    }
+});
+
+Request.JSON.implement({
     /**
-     * Send a remote procedure call.
-     * If the remoteMethod parameter is falsy, it will do nothing.
-     * 
-     * @param remoteMethod name od the remote method to invoke.
-     * @param params an array of parameters to pass (named parameters not supported).
-     * @param id optional id of the call; defaults to String.uniqueID().
+     * Send a JSON-RPC request.
+     *
+     * @param method Name of the remote method.
+     * @param params An object or array containing parameters passed to remote method.
+     * @param id Optional id of the RPC call (defaults to a random string).
      */
-    send: function(remoteMethod, params, id) {
-        if (remoteMethod) {
-            this.rpcid = id || String.uniqueID();
-            this.Request.send(
-                JSON.encode({
-                    version: JSONRPC.version,
-                    method: remoteMethod,
-                    params: params,
-                    id: this.rpcid
-                })
-            );
+    'sendRPC': function(method, params, id){
+        var rpcver = this.options.rpcVersion, data = {};
+        
+        data.method = method;
+        data.id = id || String.uniqueID();
+        
+        // version (or jsonrpc) key is only required for 
+        // JSON-RPC specification versions 1.1 and later 
+        if (rpcver == '1.1') {
+            data.version = '1.1';
         }
+        else 
+            if (rpcver == '2.0') {
+                data.jsonrpc = '2.0';
+            }
+        
+        if (!rpcver && $type(params) != 'array') {
+            // Ensure the params are an array (not needed in 2.0)
+            data.params = [params];
+        }
+        else {
+            data.params = params;
+        }
+        
+        this.send(JSON.encode(data));
     }
 });
